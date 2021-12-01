0d42803ba2abc3f8b76bd3d2b9dcb8e9e293b487
renfakai <renfakai@58.com>
2021-12-01 16:29:10 +0800

    feat: 1201

---
+++ linked_list/Twitter_test.go
@@ -0,0 +1,18 @@
+package linked_list
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestTwitter_PostTweet(t *testing.T) {
+	var a []int
+	var b = make([]int, 0)
+	var c = []int{1, 2, 3}
+	d := []int{1, 2, 3}
+
+	fmt.Println(a)
+	fmt.Println(b)
+	fmt.Println(c)
+	fmt.Println(d)
+}
